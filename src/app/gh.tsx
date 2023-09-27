d4c48e085d5a6bedb39cf8f0433850feaa98a173
C.C <fanweixiao@gmail.com>
2023-09-27 10:19:18 +0800

    turn off strict mode

--- src/app/gh.tsx
+++ src/app/gh.tsx
@@ -3,15 +3,13 @@
 import GroupHug from '@yomo/group-hug-react';
 import { createPresence } from '@yomo/presence';
 
-const id = (new Date).valueOf().toString(36)
-const pp = createPresence('https://lo.yomo.dev:8443/v1', {
-  publicKey: 'pk_live',
-  id: id,
-  debug: true,
-})
-console.log('pp------------->', id)
-
 export default function GH() {
+  const id = (new Date).valueOf().toString(36)
+  const pp = createPresence('https://lo.yomo.dev:8443/v1', {
+    publicKey: 'PiHYRKapgfmHxthWmWlqpAcdYdxCtPtl2jmwrzig',
+    id: id,
+    debug: true,
+  })
 
   return (
     <div>
